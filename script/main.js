e2b6a9d533ccb1b5d72647757b966733238979cf
Alex Fisher <cross9986@gmail.com>
2016-06-07 18:18:07 +0300

    Small visual improvements

--- script/main.js
+++ script/main.js
@@ -1,7 +1,17 @@
-var button = document.getElementById('buttons_wrap'),
-		frame = document.getElementById('window');
+;(function(){
+	var button = document.getElementById('buttons_wrap'),
+			frame = document.getElementById('window');
 
-button.addEventListener('click', function(event) {
-	var url = event.target.getAttribute('data-url');
-	frame.setAttribute('src', url);
-}, false);
+	button.addEventListener('click', function(event) {
+		var url = event.target.getAttribute('data-url');
+
+		var htmlClass = event.target.className;		
+		for (var i = 0; i < button.childNodes.length; i++) {
+			button.childNodes[i].className = htmlClass;
+		};
+
+		event.target.className = htmlClass + ' ' + 'active';
+
+		frame.setAttribute('src', 'http://www.tvmd.info/' + url);
+	});
+})();
